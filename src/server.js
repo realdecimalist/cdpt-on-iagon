0be769616f188da6533935c06c8b8a9d072d9031
realdecimalist <decimalist@protonmail.com>
2024-07-09 13:12:11 -0600

    updated dApp url

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.post('/discord/token', async (req, res) => {
         client_secret: CLIENT_SECRET,
         grant_type: 'authorization_code',
         code: req.body.code,
-        redirect_uri: 'https://shrill-queen-0263.iagon.io/',
+        redirect_uri: 'https://cool-grass-2491.iagon.io/',
     });
 
     console.log('Requesting token with params:', params.toString());
